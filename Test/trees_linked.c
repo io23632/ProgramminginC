3f26e8ea8c530c2b8f5caf17cf44a113d07111ae
Admin <admin@Admins-iMac.local>
2023-12-15 16:50:17 +0000

    insert function added to trees.c

--- Test/trees_linked.c
+++ Test/trees_linked.c
@@ -1,1 +1,36 @@
-#include <stdio.h>
+#include "/Users/admin/Desktop/Hamza/ProgramminginC/ADTs/General/general.c"
+
+typedef struct dataframe{
+    int i;
+    struct dataframe* left;
+    struct daraframe* right;
+}dataframe;
+
+
+typedef struct root{
+    dataframe* root;
+}root;
+
+
+dataframe* _insert(dataframe* t, int d);
+
+dataframe* _insert(dataframe* t, int d)
+{
+    if (t == NULL){
+        dataframe* f = (dataframe*)calloc(1, sizeof(dataframe));
+        f->i = d;
+        f->left = NULL;
+        f->right = NULL;
+        return f;
+    }
+
+    if (d < t->i){
+        t->left = _insert(t->left, d);
+    }
+
+    else if (d > t->right) {
+        t->right = _insert(t->right, d);
+    }
+
+    return t;
+}
