65163f9f02c6309c4ed72575d0dda071a0d6b025
Shahida Aslam <shahidaaslam@MacBook-Pro.local>
2023-12-18 16:57:24 +0000

    trees.c

--- Test/trees_linked.c
+++ Test/trees_linked.c
@@ -12,6 +12,7 @@ typedef struct root{
 }root;
 
 dataframe* _insert(dataframe* t, int d);
+bool _isin(dataframe* t, int d);
 
 dataframe* _insert(dataframe* t, int d)
 {
@@ -33,3 +34,35 @@ dataframe* _insert(dataframe* t, int d)
 
     return t;
 }
+
+bool _isin(dataframe* t, int d)
+{
+    /*If tree is NULL (empty) return false*/
+    if (t == NULL){
+        return false;
+    }
+
+    /*t->i is the current node of the tree, so if d is the current node 
+    then return true*/
+    if (t->i == d){
+        return true;
+    }
+
+    /*if however d is not the current note then either d is less than the current node, in which case
+    go to the left of the tree (via t->left) and call the function again which will then look at the next 
+    node (now the next node is the current one) and evaluate the condition above */
+
+
+    if (d < t->i){
+        return _isin(t->left, d);
+    }
+
+    /* else if d is bigger than the current node than the recurisve procerss repeats */
+    
+    else {
+        return _isin(t->right, d);
+    }
+
+return false;
+}
+
