e920d9a4b5b0571f2c1c4451cb8a54e5c3e92850
io23632 <io23632@bristol.ac.uk>
2024-01-18 19:07:11 +0000

    v4

--- Turtle_assignment/Turtle_Simple/turtle_v4.c
+++ Turtle_assignment/Turtle_Simple/turtle_v4.c
@@ -3,7 +3,7 @@
 
 int main (int argc, char * argv[]) 
 {
-    testParsePROG_ValidInput();
+
     test();
 
 if (argc < 2) {
@@ -36,6 +36,28 @@ free(p);
 return 0;
 }
 
+
+// int main(void)
+// {
+//     prog* p = (prog*)malloc(sizeof(prog));
+//     p->current_count = 0;
+//     if (p == NULL) {
+//         fprintf(stderr, "Memory allocation failure");
+//         return 1;
+//     }   
+
+//     int i = 0;
+//     while (scanf("%s", p->input[i]) == 1){
+//         i++;
+//     }
+//     parsePROG(p);
+
+//     free(p);
+//     return 0;
+// }
+
+
+
 void parsePROG(prog* p)
 {
     if (strcmp(p->input[p->current_count], "START") != 0) {
@@ -50,30 +72,41 @@ void parsePROG(prog* p)
    
 }
 
-void parseINSLST(prog* p) {
+void parseINSLST(prog* p)
+{
 
-    while (strcmp(p->input[p->current_count], "END") != 0) {
-        if (strcmp(p->input[p->current_count], "FORWARD") == 0) {
-            parseFWD(p);
-        }
-        else if (strcmp(p->input[p->current_count], "RIGHT") == 0) {
-            //parseRGT(p);
-        }
-        else if (strcmp(p->input[p->current_count], "COLOUR") == 0) {
-            //parseCOL(p);
-        }
-        else if (strcmp(p->input[p->current_count], "SET") == 0) {
-            //parseSET(p);
-        }
-        else {
-            fprintf(stderr, "INVALID TOKEN / EXPECTED AN END\n");
-            exit(1);
-        }
-
-        p->current_count++;
+    if (strcmp(p->input[p->current_count], "END") == 0) {
+        printf("Programe Finished\n");
+        exit(0);
     }
 
-    printf("Program Finished\n");
+    else if (strcmp(p->input[p->current_count], "FORWARD") == 0)
+    {
+        parseFWD(p);
+        parseINSLST(p);
+    }
+
+    else if (strcmp(p->input[p->current_count], "RIGHT") == 0)
+    {
+        parseRGT(p);
+        parseINSLST(p);
+    }
+
+    else if (strcmp(p->input[p->current_count], "COLOUR") == 0) {
+        parseCOL(p);
+        parseINSLST(p);
+    }
+
+    else if (strcmp(p->input[p->current_count], "SET") == 0 ) {
+        parseSET(p);
+        parseINSLST(p);
+
+    }
+    
+    else {
+        fprintf(stderr, "INVALID TOKEN / EXPECTED A END\n");
+        exit(1);
+    }
 }
 
 FWD parseFWD(prog* p)
@@ -106,6 +139,153 @@ FWD parseFWD(prog* p)
 
     }
 }
+
+// what an interpreter function might look like for fwd :
+//void gofwd_(FWD* f, turle* t);
+
+RGT parseRGT(prog* p)
+{
+    
+    RGT rgt_ins;
+    rgt_ins.type = INS_RGT;
+    p->current_count++;
+
+    // if there is a number following the RIGTH Instruction:
+    if (isNUMBER(p->input[p->current_count])) {
+        if (sscanf(p->input[p->current_count], "%lf", &rgt_ins.varnum.number) != 1) {
+            fprintf(stderr, "Expected a valid token number after RIGHT\n");
+            exit(1);
+        }
+        p->current_count++;
+        return rgt_ins;
+    }
+
+    // if there is a variable following the RIGTH Instruction:
+    else if (isVARIABLE(p->input[p->current_count])) {
+        rgt_ins.varnum.variable = p->input[p->current_count][1];
+        p->current_count++;
+        return rgt_ins;
+    }
+
+    else {
+        fprintf(stderr, "Error: Expected a variable or a number after 'RIGHT'\n");
+        exit(1);
+    }
+    
+}
+
+COL parseCOL(prog* p)
+{
+    COL col_ins;
+    col_ins.type = INS_COL;
+    p->current_count++;
+
+    // Check if it is a variable:
+    if (isVARIABLE(p->input[p->current_count])) {
+        col_ins.COL_postfix.variable = p->input[p->current_count][1];
+        p->current_count++;
+        return col_ins;
+    }
+    
+    // Check if it is a word:
+    else {
+
+        char* word = p->input[p->current_count];
+
+        if (word[0] == '\"' && word[strlen(word) - 1] == '\"') {
+            word++;
+            word[strlen(word) - 1] = '\0';
+        }
+
+        col_ins.COL_postfix.word.str = strdup(word);
+      
+       if (col_ins.COL_postfix.word.str == NULL) {
+            fprintf(stderr, "Memory allocation failure");
+            exit(1);
+       }
+
+    p->current_count++;
+    return col_ins;
+    }
+}
+
+SET parseSET(prog* p)
+{
+
+SET set; 
+set.type = INS_SET;
+set.postfix_count = 0;
+p->current_count++;
+
+//parse the letter and move on to parsePFIX
+if (isLetter(p->input[p->current_count])) {
+    set.letter = p->input[p->current_count][0];
+    p->current_count++;
+    
+
+    if(strcmp(p->input[p->current_count], "(") == 0) {
+        p->current_count++;
+        parsePOSTFIX(p, &set);
+        
+    }
+    
+    else {
+        fprintf(stderr, "expected a ( after letter in SET statement\n");
+        exit(1);
+    }
+}
+
+else {
+    fprintf(stderr, "Expected a letter in SET statement\n");
+    exit(1);
+}
+
+return set;
+
+}
+
+void parsePOSTFIX(prog* p, SET* set) {
+
+    while (strcmp(p->input[p->current_count], ")") != 0) {
+        if (set->postfix_count >= MAXTOKENSIZE) {
+            fprintf(stderr, "Postfix expression is too large\n");
+            exit(1);
+        }
+
+        PFix* currentPFix = &set->postfix[set->postfix_count];
+
+        if (isNUMBER(p->input[p->current_count])) {
+            if (sscanf(p->input[p->current_count], "%lf", &currentPFix->precurse.varnum.number) != 1) {
+                fprintf(stderr, "Invalid number in postfix expression\n");
+                exit(1);
+            }
+        }
+
+        else if (isVARIABLE(p->input[p->current_count])) {
+            currentPFix->precurse.varnum.variable = p->input[p->current_count][1];
+    
+        }
+        else if (isOperation(p->input[p->current_count])) {
+            currentPFix->precurse.symbol = p->input[p->current_count][0];
+            
+        }
+        else {
+            fprintf(stderr, "Expected valid token for postfix expression at %s\n", p->input[p->current_count]);
+            exit(1);
+        }
+
+        set->postfix_count++;
+        p->current_count++;
+    }
+
+    p->current_count++; // Move past the closing parenthesis
+}
+
+
+
+
+
+
 
 
 bool isOperation(const char* str)
@@ -149,17 +329,6 @@ bool isLetter(const char* str)
         }
     }
     return true;
-}
-
-void testParsePROG_ValidInput() {
-    prog p;
-    strcpy(p.input[0], "START");
-    strcpy(p.input[1], "FORWARD 5");
-    strcpy(p.input[2], "END");
-    p.current_count = 0;
-
-    parsePROG(&p);
-    assert(p.current_count > 0); // Expect the count to advance
 }
 
 void test(void) 
