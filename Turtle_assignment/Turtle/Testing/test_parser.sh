a678cb14ba48943b4e1450f77a3dbf9f6e044723
io23632 <io23632@bristol.ac.uk>
2024-01-19 17:49:50 +0000

    minor

--- Turtle_assignment/Turtle/Testing/test_parser.sh
+++ Turtle_assignment/Turtle/Testing/test_parser.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-# Run the makefile (assumes 'make' both compiles and runs the tests)
+cd ..
 make run
 
-# Capture the exit status
 exit_status=$?
 
-# Check if the exit status is greater than 0
 if [ $exit_status -ne 0 ]; then
     echo "Error: One or more tests failed with status $exit_status"
 else
